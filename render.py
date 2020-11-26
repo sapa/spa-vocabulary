a2117632f05581deb5c943d86761d8f1d7a345ed
Birk Weiberg <birk@keyframe.org>
2020-11-26 15:41:09 +0100

    improved support for file, video and audio formats

--- render.py
+++ render.py
@@ -8,8 +8,6 @@ import datetime
 import pytz
 
 
-df = pd.read_excel('source/ControlledTerms.xlsx', 'skos')
-
 vocab_ns = Namespace('http://vocab.performing-arts.ch/')
 
 label_columns = []
@@ -37,6 +35,8 @@ vocabs['rico'] = Namespace('https://www.ica.org/standards/RiC/ontology#')
 vocabs['ric-rst'] = Namespace('https://www.ica.org/standards/RiC/vocabularies/recordSetTypes#')
 vocabs['ebucore'] = Namespace('https://www.ebu.ch/metadata/ontologies/ebucore#')
 vocabs['eclap'] = Namespace('http://www.eclap.eu/schema/eclap/')
+vocabs['dct'] = Namespace('http://purl.org/dc/terms/')
+vocabs['premis'] = Namespace('http://www.loc.gov/premis/rdf/v1#')
 
 
 ds = Dataset()
@@ -56,68 +56,81 @@ graph.add((ldp, vocabs['prov'].generatedAtTime, Literal(
 
 uris = []
 
-scheme = None
-for idx, row in df.iterrows():
-    uri = None
-    # new scheme?
-    if not pd.isnull(row['skos_ConceptScheme']):
-        # save old graph first?
-        scheme = row['skos_ConceptScheme'].strip()
-        scheme_uri = uri = URIRef(vocab_ns[scheme])
-        try:
-            assert uri not in uris
-        except AssertionError:
-            print(f'{uri} already exists!')
-        uris.append(uri)
-        graph.add((scheme_uri, RDF.type, SKOS.ConceptScheme))
-        scheme_ns = Namespace(scheme_uri + '/')
-    # concept
-    elif not pd.isnull(row['skos_Concept']) and scheme_ns:
-        uri = URIRef(vocab_ns[f'{scheme}{row["skos_Concept"].strip()}'])
-        try:
-            assert uri not in uris
-        except AssertionError:
-            print(f'{uri} already exists!')
-        uris.append(uri)
-        graph.add((uri, RDF.type, SKOS.Concept))
-        graph.add((uri, RDF.type, URIRef('http://www.cidoc-crm.org/cidoc-crm/E55_Type')))
-        if not pd.isnull(row['additional_class']):
-            p = row['additional_class'].strip().split(':')[0]
-            q = row['additional_class'].strip().split(':')[1]
-            if p in vocabs:
-                graph.add((uri, RDF.type, URIRef(vocabs[p] + q)))
+for sheet in ['skos', 'formats']:
+    df = pd.read_excel('source/ControlledTerms.xlsx', sheet, dtype=str)
 
-        graph.add((uri, SKOS.inScheme, scheme_uri))
-        # broader, narrower
-        if not pd.isnull(row['broader']):
-            if row['broader'].strip() == '-':
-                graph.add((uri, SKOS.topConceptOf, scheme_uri))
-                graph.add((scheme_uri, SKOS.hasTopConcept, uri))
-            else:
-                # broader_uri = URIRef(scheme_ns[row['broader'].strip()])
-                broader_uri = URIRef(vocab_ns[f'{scheme}{row["broader"].strip()}'])
-                graph.add((uri, SKOS.broader, broader_uri))
-                graph.add((broader_uri, SKOS.narrower, uri))
-    # concepts and schemes
-    if not pd.isnull(row['rdfs_comment_en']):
-        graph.add((uri, RDFS.comment, Literal(
-            row['rdfs_comment_en'].strip(), lang='en')))
-    for c in label_columns:
-        if c in row and not pd.isnull(row[c]) and row[c].strip() != '-':
-            ns, pred, lang = c.split('_')
-            for s in row[c].split(';'):
-                graph.add((uri, SKOS[pred], Literal(s.strip(), lang=lang)))
-    # same as
-    if not pd.isnull(row['skos_exactMatch']) and row['skos_exactMatch'].strip() != '-':
-        for s in row['skos_exactMatch'].split(';'):
-            s = s.strip()
-            if s[:4] == 'http':
-                graph.add((uri, SKOS.exactMatch, URIRef(s)))
-            elif len(s.split(':'))==2:
-                p = s.split(':')[0]
-                q = s.split(':')[1]
-                if p in vocabs:
-                    graph.add((uri, SKOS.exactMatch, URIRef(vocabs[p] + q)))
+    scheme = None
+    for idx, row in df.iterrows():
+        uri = None
+        # new scheme?
+        if not pd.isnull(row['skos_ConceptScheme']):
+            # save old graph first?
+            scheme = row['skos_ConceptScheme'].strip()
+            scheme_uri = uri = URIRef(vocab_ns[scheme])
+            try:
+                assert uri not in uris
+            except AssertionError:
+                print(f'{uri} already exists!')
+            uris.append(uri)
+            graph.add((scheme_uri, RDF.type, SKOS.ConceptScheme))
+            scheme_ns = Namespace(scheme_uri + '/')
+        # concept
+        elif not pd.isnull(row['skos_Concept']) and scheme_ns:
+            uri = URIRef(vocab_ns[f'{scheme}{row["skos_Concept"].strip()}'])
+            try:
+                assert uri not in uris
+            except AssertionError:
+                print(f'{uri} already exists!')
+            uris.append(uri)
+            graph.add((uri, RDF.type, SKOS.Concept))
+            if sheet == 'skos':
+                graph.add((uri, RDF.type, URIRef('http://www.cidoc-crm.org/cidoc-crm/E55_Type')))
+            if not pd.isnull(row['additional_class']):
+                for c in row['additional_class'].split(';'):
+                    p = c.strip().split(':')[0]
+                    q = c.strip().split(':')[1]
+                    if p in vocabs:
+                        graph.add((uri, RDF.type, URIRef(vocabs[p] + q)))
+
+            graph.add((uri, SKOS.inScheme, scheme_uri))
+            # broader, narrower
+            if not pd.isnull(row['broader']):
+                if row['broader'].strip() == '-':
+                    graph.add((uri, SKOS.topConceptOf, scheme_uri))
+                    graph.add((scheme_uri, SKOS.hasTopConcept, uri))
+                else:
+                    # broader_uri = URIRef(scheme_ns[row['broader'].strip()])
+                    broader_uri = URIRef(vocab_ns[f'{scheme}{row["broader"].strip()}'])
+                    graph.add((uri, SKOS.broader, broader_uri))
+                    graph.add((broader_uri, SKOS.narrower, uri))
+        # concepts and schemes
+        if not pd.isnull(row['rdfs_comment_en']):
+            graph.add((uri, RDFS.comment, Literal(
+                row['rdfs_comment_en'].strip(), lang='en')))
+        for c in label_columns:
+            if c in row and not pd.isnull(row[c]) and row[c].strip() != '-':
+                ns, pred, lang = c.split('_')
+                if not pd.isnull(row[c]):
+                    for s in row[c].split(';'):
+                        graph.add((uri, SKOS[pred], Literal(s.strip(), lang=lang)))
+        # same as
+        if not pd.isnull(row['skos_exactMatch']) and row['skos_exactMatch'].strip() != '-':
+            for s in row['skos_exactMatch'].split(';'):
+                s = s.strip()
+                if s[:4] == 'http':
+                    graph.add((uri, SKOS.exactMatch, URIRef(s)))
+                elif len(s.split(':'))==2:
+                    p = s.split(':')[0]
+                    q = s.split(':')[1]
+                    if p in vocabs:
+                        graph.add((uri, SKOS.exactMatch, URIRef(vocabs[p] + q)))
+        # format extras
+        if sheet == 'formats':
+            if not pd.isnull(row['premis_formatVersion']) and row['premis_formatVersion'].strip() != '':
+                graph.add((uri, URIRef(f'{vocabs["premis"]}formatVersion'), Literal(row['premis_formatVersion'].strip())))
+            if not pd.isnull(row['ebucore_hasMimeType']) and row['ebucore_hasMimeType'].strip() != '':
+                graph.add((uri, URIRef(f'{vocabs["ebucore"]}hasMimeType'), Literal(row['ebucore_hasMimeType'].strip())))
+
 
 with open('target/vocabulary.ttl', 'wb') as f:
     f.write(graph.serialize(format='turtle'))
